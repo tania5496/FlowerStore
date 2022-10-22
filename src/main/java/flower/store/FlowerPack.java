e3d8aa7fcbd8eb3ae201dbf457782f3811f3150d
tania5496 <87234112+tania5496@users.noreply.github.com>
2022-10-22 15:05:23 +0300

    Update FlowerPack.java

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -3,4 +3,7 @@ package flower.store;
 public class FlowerPack {
     private Flower flower;
     private int quantity;
+    public double getPrice(){
+        return quantity*flower.getPrice();
+    }
 }
