3871d8d698ad7038e22a2e5f7be3819ee588e57e
tania5496 <87234112+tania5496@users.noreply.github.com>
2023-01-04 03:13:10 +0200

    Update store.java

--- src/main/java/flower/store/store.java
+++ src/main/java/flower/store/store.java
@@ -1,15 +1,25 @@
 package flower.store;
+
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
-public class store {
-    private ArrayList<FlowerPack> listOfPacks = new ArrayList<>();
-    public void addPack(FlowerPack flowerPack){
-        listOfPacks.add(flowerPack);
+import java.util.List;
+
+public class Store {
+    @Getter@Setter
+    private List<Flower> flowerAssortment = new ArrayList<>();
+
+    public void addFlower(Flower flower) {
+        flowerAssortment.add(flower);
     }
-    public double getPrice(){
-        int totalPrice = 0;
-        for(int i=0; i< listOfPacks.size(); i++){
-            totalPrice+= listOfPacks.get(i).getPrice();
+    public boolean search(Flower wantedFlower) {
+        for (Flower flower: flowerAssortment) {
+            if (wantedFlower.getType().equals(flower.getType()) && wantedFlower.getColor().equals(flower.getColor())
+                    && wantedFlower.getPrice() == flower.getPrice()) {
+                return true;
+            }
         }
-        return totalPrice;
+        return false;
     }
 }
